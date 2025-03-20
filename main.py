06b01ed516182be086541284efb4693013d4d7ec
ғᴀʟʑʏ <83463829+spettacolo@users.noreply.github.com>
2025-03-20 17:38:24 +0100

    Add admin filter and refactor approval logic in main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from pyrogram import Client, filters, enums, compose
 from pyrogram.types import Message, User, InputPrivacyRuleAllowUsers, InputPrivacyRuleAllowContacts
-from pyrogram.raw.functions
 from dotenv import load_dotenv
 import asyncio, time, os
 
@@ -13,12 +12,17 @@ account = Client("account", api_id=api_id, api_hash=api_hash)
 
 waiting_list = [] # dict list [{'id':eta,...}]
 
-async def approve():
+async def is_admin_filter(user: User):
+  return user.id == 7314203962
+
+is_admin = filters.create(is_admin_filter)
+
+async def approve(user_id: int):
   global account
   rules = await account.get_privacy(enums.PrivacyKey.NO_PAID_MESSAGES)
   users = []
   for rule in rules:
-    if rule.type == enums.PrivacyRuleType.ALLOW
+    if rule.type == enums.PrivacyRuleType.ALLOW:
       users.extend([user.id for user in rules.users])
       users.append(user_id)
       break
@@ -30,12 +34,12 @@ async def approve():
     ]
   )
 
-@bot.on_message(filters.private & filters.command(["start", "help"])
+@bot.on_message(filters.private & filters.command(["start", "help"]))
 async def start_handler(bot: Client, message: Message):
   await message.reply("Hey! Send /request {reason} to unlock the chat with @balestra.")
 
 
-@bot.on_message(filters.private & filters.command(["r", "req", "request"])
+@bot.on_message(filters.private & filters.command(["r", "req", "request"]))
 async def request_handler(bot: Client, message: Message):
   global waiting_list
   user = message.from_user
@@ -45,13 +49,13 @@ async def request_handler(bot: Client, message: Message):
   }
   waiting_list.append()
 
-@bot.on_message(filters.private & is_admin & filters.command(["approve", "ok", "allow"])
+@bot.on_message(filters.private & is_admin & filters.command(["approve", "ok", "allow"]))
 async def approve_handler(bot: Client, message: Message):
   if len(message.text.split()) > 1:
     command, user_id = message.text.split()
     try:
       await bot.get_users(user_id)
-      await allow()
+      await approve()
     except Exception as e:
       print(f"Error while approving: {e}")
 

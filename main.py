c48b3161f2391d848a3c034a863ac949d3a6d2ce
ғᴀʟʑʏ <83463829+spettacolo@users.noreply.github.com>
2025-03-20 17:29:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from pyrogram import Client, filters
-from pyrogram.types import Message, User
+from pyrogram import Client, filters, enums, compose
+from pyrogram.types import Message, User, InputPrivacyRuleAllowUsers, InputPrivacyRuleAllowContacts
+from pyrogram.raw.functions
 from dotenv import load_dotenv
 import asyncio, time, os
 
@@ -12,13 +13,30 @@ account = Client("account", api_id=api_id, api_hash=api_hash)
 
 waiting_list = [] # dict list [{'id':eta,...}]
 
+async def approve():
+  global account
+  rules = await account.get_privacy(enums.PrivacyKey.NO_PAID_MESSAGES)
+  users = []
+  for rule in rules:
+    if rule.type == enums.PrivacyRuleType.ALLOW
+      users.extend([user.id for user in rules.users])
+      users.append(user_id)
+      break
+  await account.set_privacy(
+    enums.PrivacyKey.NO_PAYD_MESSAGES,
+    rules = [
+      InputPrivacyRuleAllowUsers(users),
+      InputPrivacyRuleAllowContacts()
+    ]
+  )
+
 @bot.on_message(filters.private & filters.command(["start", "help"])
-async def start(bot: Client, message: Message):
+async def start_handler(bot: Client, message: Message):
   await message.reply("Hey! Send /request {reason} to unlock the chat with @balestra.")
 
 
 @bot.on_message(filters.private & filters.command(["r", "req", "request"])
-async def start(bot: Client, message: Message):
+async def request_handler(bot: Client, message: Message):
   global waiting_list
   user = message.from_user
   user_dict = {
@@ -27,9 +45,22 @@ async def start(bot: Client, message: Message):
   }
   waiting_list.append()
 
+@bot.on_message(filters.private & is_admin & filters.command(["approve", "ok", "allow"])
+async def approve_handler(bot: Client, message: Message):
+  if len(message.text.split()) > 1:
+    command, user_id = message.text.split()
+    try:
+      await bot.get_users(user_id)
+      await allow()
+    except Exception as e:
+      print(f"Error while approving: {e}")
+
 @bot.on_message(filters.private)
-async def wait_for_response(bot: Client, message: Message):
+async def wait_for_response_handler(bot: Client, message: Message):
   pass
 
+async def main(bot: Client, account: Client):
+  await compose([bot, account])
+
 if __name__ == "__main__":
-  bot.run()
+  asyncio.run(main(bot, account))

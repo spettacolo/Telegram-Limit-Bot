16a86ed663cc288ce1f0517a622e43e8616be41c
ғᴀʟʑʏ <83463829+spettacolo@users.noreply.github.com>
2025-04-12 00:12:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from pyrogram import Client, filters, compose, enums, types
-from pyrogram.types import Message, User, InputPrivacyRuleAllowUsers, InputPrivacyRuleAllowContacts
+from pyrogram.types import Message, User, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from dotenv import load_dotenv
 import asyncio, time, os
 
@@ -12,6 +12,7 @@ bot_token = os.getenv("BOT_TOKEN")
 owner = os.getenv("OWNER")
 
 bot = Client("bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
+bot_data = None
 
 async def is_admin_filter(_, __, message: Message):
   user = message.from_user
@@ -25,7 +26,25 @@ async def start_handler(bot: Client, message: Message):
 
 @bot.on_message(filters.private)
 async def new_message_handler(bot: Client, message: Message):
-  pass
+  global bot_data
+  reply_markup = InlineKeyboardMarkup(
+    [
+      [InlineKeyboardButton("View Profile", user_id=message.from_user.id)],
+      #[InlineKeyboardButton("Send Message", callback_data=f"send_message_{message.from_user.id}")],
+      [InlineKeyboardButton("Reply", url=f"t.me/{bot_data.username}?start={message.from_user.id},{message.id}")],
+    ]
+  )
+
+  await bot.send_message(owner, f"New message from {message.from_user.first_name}:\n\n{message.text}", reply_markup=reply_markup)
+  await message.reply("Message sent to owner.")
+
+async def setup():
+  global bot_data
+  await bot.start()
+  bot_data = await bot.get_me()
+  await bot.stop()
+  print("Bot data retrieved.")
 
 if __name__ == "__main__":
+  asyncio.run(setup())
   bot.run()
